08ab0fb5918d94ccb4ab59f8be0ce87de29eb9fe
Edelin11 <espinoedelin@gmail.com>
2023-02-28 16:42:23 -0500

    Update to the documentation

--- FlipWord/flip.js
+++ FlipWord/flip.js
@@ -1,29 +1,26 @@
+/* call the cards class */
 const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false;
+let hasFlippedCard = false; 
 let lockBoard = false;
 let firstCard, secondCard;
 
+/* flip card function to  */
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add('flip');
-
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-
+  if (lockBoard) return; /* if you're using this board, prevent other cards to flip  */
+  if (this === firstCard) return; /* store card on flipCard()  */
+  this.classList.add('flip'); // add css animation to card on flipCard()
+  if (!hasFlippedCard) { // if not true, set true and return flipped card.
+    hasFlippedCard = true; 
+    firstCard = this; // this is the flipped card which is then returned
     return;
   }
-
-  secondCard = this;
+  secondCard = this; // if it is true, check second card, or asign the second card flipped to flipCard()
   checkForMatch();
 }
 
-function checkForMatch() {
+function checkForMatch() { /* check if card flipped one matches second flipped card */
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
   isMatch ? disableCards() : unflipCards();
 }
 
