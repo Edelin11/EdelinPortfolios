c03ddd409b1fdef04fc9ec2db1ee2082c316f6ef
Edelin11 <espinoedelin@gmail.com>
2023-03-24 14:11:58 -0400

    New UI Design for Calculator

--- logic.js
+++ logic.js
@@ -27,21 +27,6 @@
       }
   }
   
-  /* READ MORE PART */
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-  
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.display === "block") {
-        content.style.display = "none";
-      } else {
-        content.style.display = "block";
-      }
-    });
-  }
   
   // THIS IS FOR THE IMG SLIDERS
   let slideIndex = 1;
