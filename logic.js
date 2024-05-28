47f535557ce8466b95d88ad9578c193ec5647c27
Edelin11 <espinoedelin@gmail.com>
2024-05-27 20:51:45 -0400

    Change in Profile style
    I keep learning and I keep trying new things. It's starting to look like I've vision it.

--- logic.js
+++ logic.js
@@ -11,7 +11,7 @@
   function myFunction() {
       var navbar = document.getElementById("myNavbar");
       if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-          navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-black";
+          navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" ;
       } else {
           navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
       }
